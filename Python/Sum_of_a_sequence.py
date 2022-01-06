8489bdc491b53d0953ff835fd2a0e51111a7760a
Nikita Yarosh <ThRuWol@yandex.ru>
2022-01-06 20:19:15 +0300

    Create Sum_of_a_sequence.py

---
+++ Python/Sum_of_a_sequence.py
@@ -0,0 +1,4 @@
+# https://www.codewars.com/kata/586f6741c66d18c22800010a/train/python
+
+def sequence_sum(begin_number, end_number, step):
+    return sum([i for i in range(begin_number, end_number+1, step)])
