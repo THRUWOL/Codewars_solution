d11c44840a759cfcb8da4bf3d99e0eade990d853
Nikita Yarosh <yarosh.nv@yandex.ru>
2022-03-12 16:47:12 +0300

    Create Correct the mistakes of the character recognition software.java

---
+++ Java/Correct the mistakes of the character recognition software.java
@@ -0,0 +1,7 @@
+// https://www.codewars.com/kata/577bd026df78c19bca0002c0/train/java
+
+public class Correct {
+  public static String correct(String string) {
+    return string.replace('1','I').replace('5','S').replace('0','O');
+  }
+}
