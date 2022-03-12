88ebd58889f8cda1cc8922be9e069511da7e559f
Nikita Yarosh <yarosh.nv@yandex.ru>
2022-03-12 16:51:37 +0300

    Create Remove exclamation marks.java

---
+++ Java/Remove exclamation marks.java
@@ -0,0 +1,7 @@
+// https://www.codewars.com/kata/57a0885cbb9944e24c00008e/train/java
+
+class Solution {
+    static String removeExclamationMarks(String s) {
+        return s.replaceAll("!","");
+    }
+}
