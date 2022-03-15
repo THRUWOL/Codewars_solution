fd992a03e39e085295bbf9455dbf384438c9a832
Nikita Yarosh <yarosh.nv@yandex.ru>
2022-03-15 18:37:05 +0300

    Create Grasshopper - Messi goals function.java

---
+++ Java/Grasshopper - Messi goals function.java
@@ -0,0 +1,7 @@
+//https://www.codewars.com/kata/55f73be6e12baaa5900000d4/train/java
+
+public class Goals {
+    public static int goals(int laLigaGoals, int copaDelReyGoals, int championsLeagueGoals) {
+        return laLigaGoals+copaDelReyGoals+championsLeagueGoals;
+    }
+}
