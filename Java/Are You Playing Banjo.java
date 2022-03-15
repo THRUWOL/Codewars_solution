806c7af2de22f1578cb67a81e7aceff30b699216
Nikita Yarosh <yarosh.nv@yandex.ru>
2022-03-15 17:13:05 +0300

    Create Are You Playing Banjo.java

---
+++ Java/Are You Playing Banjo.java
@@ -0,0 +1,12 @@
+//https://www.codewars.com/kata/53af2b8861023f1d88000832/train/java
+
+public class Banjo {
+  public static String areYouPlayingBanjo(String name) {
+    if (name.charAt(0) == 'R' || name.charAt(0) == 'r'){
+      return name + " plays banjo";
+    }
+    else {
+      return name + " does not play banjo";
+    }
+  }
+}
