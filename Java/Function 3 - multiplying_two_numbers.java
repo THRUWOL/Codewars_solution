0dc0d7eff6650dae742bce5f786b0e1bfe4b90a2
Nikita Yarosh <yarosh.nv@yandex.ru>
2022-03-15 17:08:28 +0300

    Create Function 3 - multiplying_two_numbers.java

---
+++ Java/Function 3 - multiplying_two_numbers.java
@@ -0,0 +1,7 @@
+// https://www.codewars.com/kata/523b66342d0c301ae400003b/train/java
+
+public class Kata {
+    public static int multiply(int num1, int num2) {
+        return num1*num2;
+    } 
+}
