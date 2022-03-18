4f49e73df715acff15de48532d7b324c5d0202a1
Nikita Yarosh <yarosh.nv@yandex.ru>
2022-03-18 21:56:47 +0300

    Create Grasshopper - Debug sayHello.java

---
+++ Java/Grasshopper - Debug sayHello.java
@@ -0,0 +1,8 @@
+//https://www.codewars.com/kata/5625618b1fe21ab49f00001f/train/java
+
+public class GrassHopper {
+
+    public static String sayHello(String name) {
+        return "Hello, " + name; 
+    }
+}
